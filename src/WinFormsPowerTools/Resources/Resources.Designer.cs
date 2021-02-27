c667a90cc9df2ec0284a831e0e674257f1dfb628
Klaus Löffelmann <klaus.loeffelmann@microsoft.com>
2021-02-27 15:57:34 -0800

    Rename Projects, Solution, Namespaces to 'WinFormsPowerTools'.

--- src/WinFormsPowerTools/Resources/Resources.Designer.cs
+++ src/WinFormsPowerTools/Resources/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace System.Windows.Forms.DataEntryForms.Resources {
+namespace WinFormsPowerTools.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace System.Windows.Forms.DataEntryForms.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System.Windows.Forms.DataEntryForms.Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormsPowerTools.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
