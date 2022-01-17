33f6c867c2bf127d64f6a576375dc0936fdc0966
Klaus Löffelmann <klloeffe@microsoft.com>
2022-01-16 18:08:06 -0800

    Implement basic stubs for Direct2D based Maui graphics rendering.

---
+++ src/WinformsPowerTools.Direct2D/MauiGraphics/D2DGraphicsView.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Microsoft.Maui.Graphics.D2D.WinForms
+{
+    public class D2DGraphicsView : Control
+    {
+        private IDrawable? _drawable;
+        private D2DCanvas _canvas;
+        private bool _isDesignModeOrUnknown = true;
+
+        public D2DGraphicsView()
+        {
+            ResizeRedraw = true;
+
+            _canvas = new D2DCanvas() { Window = this };
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            _canvas.Window = this;
+
+            if (!IsAncestorSiteInDesignMode)
+            {
+                _isDesignModeOrUnknown = false;
+                if (Drawable is not null)
+                {
+                    Invalidate();
+                }
+            }
+        }
+
+        protected override void OnHandleDestroyed(EventArgs e)
+        {
+            base.OnHandleDestroyed(e);
+            _isDesignModeOrUnknown = true;
+        }
+
+        public ICanvas Canvas => _canvas;
+
+        public IDrawable? Drawable
+        {
+            get => _drawable;
+            set
+            {
+                if (!Equals(_drawable,value))
+                {
+                    _drawable = value;
+                    //OnDrawableChanged();
+                }
+
+                if (_drawable is not null && IsHandleCreated)
+                {
+                    Invalidate();
+                }
+            }
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+            // We're blocking background-painting by not
+            // calling the base method, if we're not in Design Mode.
+            if (_isDesignModeOrUnknown)
+            {
+                base.OnPaintBackground(pevent);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (_isDesignModeOrUnknown)
+            {
+                return;
+            }
+
+            _canvas.BeginDraw();
+
+            if (Drawable is not null)
+            {
+                Drawable.Draw(Canvas, new RectangleF(
+                    ClientRectangle.X,
+                    ClientRectangle.Y,
+                    ClientRectangle.Width,
+                    ClientRectangle.Height));
+            }
+
+            _canvas.EndDraw();
+        }
+    }
+}
---
+++ src/WinformsPowerTools.Direct2D/MauiGraphics/D2DLayer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows.Forms;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Direct2D;
+using Windows.Win32.Graphics.Direct2D.Common;
+
+namespace Microsoft.Maui.Graphics.D2D
+{
+    internal class D2DLayer
+    {
+        private ID2D1RenderTarget? _renderTarget;
+        private IWin32Window? _window;
+
+        private Color _strokeColor = Colors.Black;
+        private ID2D1SolidColorBrush? _strokeColorCash;
+
+        public D2DLayer(IWin32Window? window)
+        {
+            _window = window;
+        }
+
+        public ID2D1RenderTarget RenderTarget => _renderTarget ??= CreateRenderTarget(_window);
+        public ID2D1Factory? Direct2DFactory { get; } = D2DExtensions.CreateFactory();
+        public IWin32Window? Window => _window;
+
+
+        public void BeginDraw()
+        {
+            RenderTarget.BeginDraw();
+        }
+
+        unsafe public void EndDraw()
+        {
+            RenderTarget.EndDraw();
+        }
+
+        unsafe public void Flush()
+        {
+            RenderTarget.Flush();
+        }
+
+        internal void DrawLine(float x1, float y1, float x2, float y2)
+        {
+            D2D_POINT_2F startPoint = new() { x = x1, y = x1 };
+            D2D_POINT_2F endPoint = new() { x = x2, y = y2 };
+
+            _renderTarget!.DrawLine(startPoint, endPoint, _strokeColorCash, 3, null);
+        }
+
+        private static ID2D1RenderTarget CreateRenderTarget(IWin32Window? window)
+        {
+            if (window is null)
+            {
+                throw new ArgumentNullException(nameof(window));
+            }
+
+            var control = Control.FromHandle(window.Handle);
+
+            if (control is null)
+            {
+                throw new InvalidOperationException("No control with the given windowsHandler found.");
+            }
+
+            var factory = D2DExtensions.CreateFactory();
+            if (factory is not null)
+            {
+                var renderTargetProperties = new D2D1_HWND_RENDER_TARGET_PROPERTIES();
+                renderTargetProperties.hwnd = new HWND(window.Handle);
+
+                var size = new D2D_SIZE_U();
+                size.width = (uint)control.Width;
+                size.height = (uint)control.Height;
+                renderTargetProperties.pixelSize = size;
+
+                factory.CreateHwndRenderTarget(default, renderTargetProperties, out var dcRenderTarget);
+                return dcRenderTarget;
+            }
+            else
+            {
+                throw new InvalidOperationException("Couldn't create a D2D render target for the given control.");
+            }
+        }
+
+        internal Color StrokeColor
+        {
+            get => _strokeColor;
+
+            set
+            {
+                if (!Equals(_strokeColor, value))
+                {
+                    D2D1_COLOR_F strokeColor;
+
+                    strokeColor.a = value.Alpha;
+                    strokeColor.b = value.Blue;
+                    strokeColor.g = value.Green;
+                    strokeColor.r = value.Red;
+
+                    RenderTarget.CreateSolidColorBrush(in strokeColor, null, out var strokeColorCache);
+                    _strokeColorCash = strokeColorCache;
+                }
+            }
+        }
+    }
+}
