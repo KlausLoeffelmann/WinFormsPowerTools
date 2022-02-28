3c11b657efcfe0a606750f98129507387e9bd830
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2022-02-27 17:54:13 -0800

    Work on bitmap sample.

---
+++ src/WinFormsPowerToolsDemo/D2DSamples/RetroVideoController/RetroVideoColorItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace WinFormsPowerToolsDemo.D2DSamples.RetroVideoController
+{
+    public struct RetroVideoColorItem
+    {
+        public RetroVideoColorItem(int index, string name, Color color)
+        {
+            Index = index;
+            Name = name;
+            Color = color;
+        }
+
+        public int Index { get; }
+        public string Name { get; }
+        public Color Color { get; }
+    }
+}
