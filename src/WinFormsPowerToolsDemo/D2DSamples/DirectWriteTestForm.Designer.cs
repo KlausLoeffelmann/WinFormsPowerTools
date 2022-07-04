427389d7a89165a0fb3167a55b839eeafb251e12
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2022-07-03 17:53:51 -0700

    Introduce sample Form to test DirectWrite implementation.

---
+++ src/WinFormsPowerToolsDemo/D2DSamples/DirectWriteTestForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsPowerToolsDemo.D2DSamples
+{
+    partial class DirectWriteTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.direct2dPanel1 = new System.Windows.Forms.Direct2D.Direct2DPanel();
+            this.SuspendLayout();
+            // 
+            // direct2dPanel1
+            // 
+            this.direct2dPanel1.Location = new System.Drawing.Point(28, 35);
+            this.direct2dPanel1.Name = "direct2dPanel1";
+            this.direct2dPanel1.Size = new System.Drawing.Size(1080, 644);
+            this.direct2dPanel1.TabIndex = 0;
+            this.direct2dPanel1.Text = "_direct2dPanel";
+            this.direct2dPanel1.PaintIGraphics += new System.EventHandler<System.Windows.Forms.Direct2D.GraphicsPaintEventArgs>(this.direct2dPanel1_PaintIGraphics);
+            // 
+            // DirectWriteTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1142, 713);
+            this.Controls.Add(this.direct2dPanel1);
+            this.Name = "DirectWriteTestForm";
+            this.Text = "DirectWriteTestForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Direct2D.Direct2DPanel direct2dPanel1;
+    }
+}
--- src/WinformsPowerTools.Direct2D/D2DWinForms/Direct2DFont.cs
+++ src/WinformsPowerTools.Direct2D/D2DWinForms/Direct2DFont.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Globalization;
 using Windows.Win32;
 using Windows.Win32.Graphics.DirectWrite;
 
@@ -31,6 +32,51 @@ namespace System.Windows.Forms.Direct2D
             return new Direct2DFont(textFormat);
         }
 
+        public static Direct2DFont FromFontAndStringFormat(
+            Font font,
+            StringFormat stringFormat,
+            IDWriteFactory writeFactory)
+        {
+            var d2dFont = FromFont(font, writeFactory);
+
+            // TODO: We need to have a cache for that, which should go into the Font-object.
+            var textFormatAlignment = stringFormat.Alignment switch
+            {
+                StringAlignment.Near => DWRITE_TEXT_ALIGNMENT.DWRITE_TEXT_ALIGNMENT_LEADING,
+                StringAlignment.Far => DWRITE_TEXT_ALIGNMENT.DWRITE_TEXT_ALIGNMENT_TRAILING,
+                StringAlignment.Center => DWRITE_TEXT_ALIGNMENT.DWRITE_TEXT_ALIGNMENT_CENTER,
+
+                _ => throw new NotImplementedException($"Text alignment '{stringFormat.Alignment}' is not supported.")
+            };
+
+            var lineFormatAlignment = stringFormat.LineAlignment switch
+            {
+                StringAlignment.Near => DWRITE_PARAGRAPH_ALIGNMENT.DWRITE_PARAGRAPH_ALIGNMENT_NEAR,
+                StringAlignment.Far => DWRITE_PARAGRAPH_ALIGNMENT.DWRITE_PARAGRAPH_ALIGNMENT_FAR,
+                StringAlignment.Center => DWRITE_PARAGRAPH_ALIGNMENT.DWRITE_PARAGRAPH_ALIGNMENT_CENTER,
+
+                _ => throw new NotImplementedException($"Text alignment '{stringFormat.LineAlignment}' is not supported.")
+            };
+
+            var trimmingGranularity = stringFormat.Trimming switch
+            {
+                StringTrimming.None => DWRITE_TRIMMING_GRANULARITY.DWRITE_TRIMMING_GRANULARITY_NONE,
+                StringTrimming.EllipsisCharacter => DWRITE_TRIMMING_GRANULARITY.DWRITE_TRIMMING_GRANULARITY_CHARACTER,
+                StringTrimming.EllipsisWord => DWRITE_TRIMMING_GRANULARITY.DWRITE_TRIMMING_GRANULARITY_WORD,
+
+                _ => throw new NotImplementedException($"Text alignment '{stringFormat.Trimming}' is not supported.")
+            };
+
+            DWRITE_TRIMMING trimming = new();
+            trimming.granularity = trimmingGranularity;
+
+            d2dFont.TextFormat.SetTextAlignment(textFormatAlignment);
+            d2dFont.TextFormat.SetParagraphAlignment(lineFormatAlignment);
+            d2dFont.TextFormat.SetTrimming(trimming, null);
+
+            return d2dFont;
+        }
+
         private static IDWriteTextFormat CreateTextFormat(
             IDWriteFactory writeFactory,
             string fontFamilyname,
@@ -40,6 +86,11 @@ namespace System.Windows.Forms.Direct2D
             DWRITE_FONT_STRETCH fontStretch = DWRITE_FONT_STRETCH.DWRITE_FONT_STRETCH_NORMAL,
             string? localName = null)
         {
+            if (string.IsNullOrEmpty(localName))
+            {
+                localName = CultureInfo.CurrentCulture.Name;
+            }
+
             writeFactory.CreateTextFormat(
                 fontFamilyname,
                 fontCollection: null,
