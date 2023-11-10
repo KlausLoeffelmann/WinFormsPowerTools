ab850a69c50d59bd0fdae2f2225654c981535303
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2023-11-10 13:56:00 -0800

    Refactor ToolStripSymbolMenuItem to use static interfaces.

---
+++ src/WinForms.PowerTools.Controls/Controls/IToolStripItemSymbolProvider.cs
@@ -0,0 +1,207 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace WinForms.PowerTools.Controls;
+
+internal interface IToolStripItemSymbolProvider
+{
+    /// <summary>
+    ///  Occurs when the <see cref="Symbol"/> property value changes.
+    /// </summary>
+    event EventHandler? SymbolChanged;
+
+    /// <summary>
+    ///  Occurs when the <see cref="SymbolColor"/> property value changes.
+    /// </summary>
+    event EventHandler? SymbolColorChanged;
+
+    /// <summary>
+    ///  Occurs when the <see cref="SymbolSize"/> property value changes.
+    /// </summary>
+    event EventHandler? SymbolSizeChanged;
+
+    /// <summary>
+    ///  Occurs when the <see cref="SymbolOffset"/> property value changes.
+    /// </summary>
+    event EventHandler? SymbolOffsetChanged;
+
+    /// <summary>
+    ///  Occurs when the <see cref="SymbolScaling"/> property value changes.
+    /// </summary>
+    event EventHandler? SymbolScalingChanged;
+
+    private static readonly Color s_transparentColor = Color.Transparent;
+
+    /// <summary>
+    ///  Gets or sets the symbol character.
+    /// </summary>
+    SegoeFluentIcons? Symbol { get; set; }
+
+    /// <inheritdoc/>
+    Image? Image { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the symbol scaling in percent (100 = no scaling.)
+    /// </summary>
+    int SymbolScaling { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the symbol color.
+    /// </summary>
+    Color SymbolColor { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the symbol size.
+    /// </summary>
+    Size? SymbolSize { get; set; }
+
+    /// <summary>
+    ///  Gets or sets the symbol offset.
+    /// </summary>
+    Size SymbolOffset { get; set; }
+
+    [AllowNull]
+    protected SymbolSource<SegoeFluentIcons> SymbolSource { get; set; }
+    protected SymbolImageFactory? SymbolImageFactory { get; set; }
+
+    protected static SymbolSource<SegoeFluentIcons> SymbolSourceGetter(IToolStripItemSymbolProvider provider) 
+        => provider.SymbolSource;
+
+    protected static SymbolSource<SegoeFluentIcons> SymbolSourceSetter(
+        IToolStripItemSymbolProvider provider, 
+        SymbolSource<SegoeFluentIcons> symbolSource) 
+            => provider.SymbolSource=symbolSource;
+
+    protected static void SymbolSetter(
+        IToolStripItemSymbolProvider provider,
+        Action<EventArgs> onSymbolChanged,
+        SegoeFluentIcons? value)
+    {
+        ArgumentNullException.ThrowIfNull(provider);
+        ArgumentNullException.ThrowIfNull(onSymbolChanged);
+
+        SegoeFluentIcons? symbol = provider.SymbolSource.HasSymbolValue
+            ? provider.SymbolSource.Symbol
+            : null;
+
+        if (symbol == value)
+        {
+            return;
+        }
+
+        if (!value.HasValue)
+        {
+            provider.SymbolSource.SetSymbolNull();
+            UpdateSymbolImageFactory(provider);
+            onSymbolChanged(EventArgs.Empty);
+            return;
+        }
+
+        provider.SymbolSource.Symbol = value.Value;
+        UpdateSymbolImageFactory(provider);
+        onSymbolChanged(EventArgs.Empty);
+    }
+
+    protected static void SymbolScalingSetter(
+        IToolStripItemSymbolProvider provider,
+        Action<EventArgs> onSymbolScalingChanged,
+        int value,
+        ref int symbolScaling)
+    {
+        ArgumentNullException.ThrowIfNull(provider);
+        ArgumentNullException.ThrowIfNull(onSymbolScalingChanged);
+
+        if (symbolScaling == value)
+        {
+            return;
+        }
+
+        if (value < 25 || value > 300)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), value, "Value must be between 25 and 300.");
+        }
+
+        symbolScaling = value;
+        UpdateSymbolImageFactory(provider);
+        onSymbolScalingChanged(EventArgs.Empty);
+    }
+
+    protected static void SymbolColorSetter(
+        IToolStripItemSymbolProvider provider,
+        Action<EventArgs> onSymbolColorChanged,
+        Color value,
+        ref Color symbolColor)
+    {
+        ArgumentNullException.ThrowIfNull(provider);
+        ArgumentNullException.ThrowIfNull(onSymbolColorChanged);
+
+        if (symbolColor == value)
+        {
+            return;
+        }
+
+        symbolColor = value;
+        UpdateSymbolImageFactory(provider);
+        onSymbolColorChanged(EventArgs.Empty);
+    }
+
+    protected static void SymbolSizeSetter(
+        IToolStripItemSymbolProvider provider,
+        Action<EventArgs> onSymbolSizeChanged,
+        Size? value,
+        ref Size? size)
+    {
+        ArgumentNullException.ThrowIfNull(provider);
+        ArgumentNullException.ThrowIfNull(onSymbolSizeChanged);
+
+        if (size == value)
+        {
+            return;
+        }
+
+        size = value;
+        UpdateSymbolImageFactory(provider);
+        onSymbolSizeChanged(EventArgs.Empty);
+    }
+
+    protected static void SymbolOffsetSetter(
+        IToolStripItemSymbolProvider provider,
+        Action<EventArgs> onSymbolOffsetChanged,
+        Size value,
+        ref Size symbolOffset)
+    {
+        ArgumentNullException.ThrowIfNull(provider);
+        ArgumentNullException.ThrowIfNull(onSymbolOffsetChanged);
+
+        if (symbolOffset == value)
+        {
+            return;
+        }
+
+        symbolOffset = value;
+        UpdateSymbolImageFactory(provider);
+        onSymbolOffsetChanged(EventArgs.Empty);
+    }
+
+    protected static void UpdateSymbolImageFactory(IToolStripItemSymbolProvider provider)
+    {
+        if (!provider.SymbolSource.HasSymbolValue || !(provider.SymbolSize.HasValue))
+        {
+            provider.SymbolImageFactory = null;
+            provider.Image = null!;
+            return;
+        }
+
+        provider.SymbolImageFactory = new SymbolImageFactory(
+            (char)provider.SymbolSource.Symbol,
+            provider.SymbolSource.FontName,
+            provider.SymbolSize.Value.Width,
+            provider.SymbolSize.Value.Height,
+            provider.SymbolScaling,
+            provider.SymbolColor,
+            s_transparentColor,
+            provider.SymbolOffset.Width,
+            provider.SymbolOffset.Height);
+
+        provider.Image = provider.SymbolImageFactory.SymbolImage;
+    }
+}
--- src/WinForms.PowerTools.Controls/Controls/ToolStripSymbolMenuItem.cs
+++ src/WinForms.PowerTools.Controls/Controls/ToolStripSymbolMenuItem.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace WinForms.PowerTools.Controls;
 
@@ -6,45 +7,41 @@ namespace WinForms.PowerTools.Controls;
 ///  Represents a menu item with a symbol.
 /// </summary>
 [ToolboxBitmap(typeof(ToolStripMenuItem))]
-public class ToolStripSymbolMenuItem : ToolStripMenuItem
+public class ToolStripSymbolMenuItem : ToolStripMenuItem, IToolStripItemSymbolProvider
 {
     private Color _symbolColor=Color.Black;
-    private readonly Color _transparentColor = Color.Transparent;
+    private static readonly Color s_transparentColor = Color.Transparent;
     private Size? _symbolSize = new Size(32, 32);
     private Size _symbolOffset;
-    private int _scalePercentage = 100;
-    private readonly SymbolSource<SegoeFluentIcons> _symbolSource;
+    private int _symbolScaling = 100;
 
     public ToolStripSymbolMenuItem() : base()
     {
         base.ImageScaling = ToolStripItemImageScaling.None;
         base.DisplayStyle = ToolStripItemDisplayStyle.ImageAndText;
         base.TextImageRelation = TextImageRelation.ImageAboveText;
-        _symbolSource = new SymbolSource<SegoeFluentIcons>("Segoe Fluent Icons");
+
+        IToolStripItemSymbolProvider.SymbolSourceSetter(
+            this,
+            new SymbolSource<SegoeFluentIcons>("Segoe Fluent Icons"));          ;
     }
 
-    /// <summary>
-    ///  Occurs when the <see cref="Symbol"/> property value changes.
-    /// </summary>
+    /// <inheritdoc/>
     public event EventHandler? SymbolChanged;
 
-    /// <summary>
-    ///  Occurs when the <see cref="SymbolColor"/> property value changes.
-    /// </summary>
+    /// <inheritdoc/>
     public event EventHandler? SymbolColorChanged;
 
-    /// <summary>
-    ///  Occurs when the <see cref="SymbolSize"/> property value changes.
-    /// </summary>
+    /// <inheritdoc/>
     public event EventHandler? SymbolSizeChanged;
 
-    /// <summary>
-    ///  Occurs when the <see cref="SymbolOffset"/> property value changes.
-    /// </summary>
+    /// <inheritdoc/>
     public event EventHandler? SymbolOffsetChanged;
 
-    public event EventHandler? ScalePercentageChanged;
+    /// <inheritdoc/>
+    public event EventHandler? SymbolScalingChanged;
 
+    /// <inheritdoc/>
     private SymbolImageFactory? _symbolImageFactory;
 
     /// <inheritdoc/>
@@ -62,133 +59,97 @@ public class ToolStripSymbolMenuItem : ToolStripMenuItem
 
     private void ResetTextImageRelation() => TextImageRelation = TextImageRelation.ImageAboveText;
 
-    public int ScalePercentage
-    {
-        get => _scalePercentage;
-        set
-        {
-            if (_scalePercentage == value)
-            {
-                return;
-            }
 
-            if (value < 25 || value > 300)
-            {
-                throw new ArgumentOutOfRangeException(nameof(value), value, "Value must be between 25 and 300.");
-            }
+    [AllowNull]
+    SymbolSource<SegoeFluentIcons> IToolStripItemSymbolProvider.SymbolSource { get; set; }
 
-            _scalePercentage = value;
-            OnScalePercentageChanged(EventArgs.Empty);
-        }
-    }
+    SymbolImageFactory? IToolStripItemSymbolProvider.SymbolImageFactory { get; set; }
 
-    /// <summary>
-    ///  Gets or sets the symbol character.
-    /// </summary>
+    private SymbolSource<SegoeFluentIcons> SymbolSource 
+        => IToolStripItemSymbolProvider.SymbolSourceGetter(this);
+
+    /// <inheritdoc/>
     [Bindable(BindableSupport.Default, BindingDirection.OneWay)]
     public SegoeFluentIcons? Symbol
     {
-        get => _symbolSource.Symbol;
+        get => SymbolSource.HasSymbolValue 
+            ? SymbolSource.Symbol 
+            : null;
 
         set
         {
-            if (_symbolSource.Symbol == value)
-            {
-                return;
-            }
-
-            if (!value.HasValue)
-            {
-                _symbolSource.SetSymbolNull();
-                _symbolImageFactory = null;
-                OnSymbolChanged(EventArgs.Empty);
-                return;
-            }
-
-            _symbolSource.Symbol = value.Value;
-            OnSymbolChanged(EventArgs.Empty);
+            IToolStripItemSymbolProvider.SymbolSetter(
+            provider: this,
+            onSymbolChanged: OnSymbolChanged,
+            value: value);
         }
     }
 
     private bool ShouldSerializeSymbol() 
-        => _symbolSource.HasSymbolValue;
+        => SymbolSource.HasSymbolValue;
 
     private void ResetSymbol() => Symbol = null;
 
-    /// <summary>
-    ///  Gets or sets the color of the symbol.
-    /// </summary>
-    /// <exception cref="InvalidOperationException">
-    ///  Thrown when <see cref="Symbol"/> is <see langword="null"/>.
-    /// </exception>
+    /// <inheritdoc/>
+    public int SymbolScaling
+    {
+        get => _symbolScaling;
+        set => IToolStripItemSymbolProvider.SymbolScalingSetter(
+            provider: this,
+            onSymbolScalingChanged: OnSymbolScalingChanged,
+            value: value,
+            ref _symbolScaling);
+    }
+
+    /// <inheritdoc/>
     public Color SymbolColor
     {
         get => _symbolColor;
 
-        set
-        {
-            if (_symbolColor == value)
-            {
-                return;
-            }
-
-            _symbolColor = value;
-            OnSymbolColorChanged(EventArgs.Empty);
-        }
+        set => IToolStripItemSymbolProvider.SymbolColorSetter(
+            provider: this,
+            onSymbolColorChanged: OnSymbolColorChanged,
+            value: value,
+            ref _symbolColor);
     }
 
     private bool ShouldSerializeSymbolColor() => _symbolColor != Color.Black;
 
     private void ResetSymbolColor() => SymbolColor = Color.Black;
 
-    /// <summary>
-    ///  Gets or sets the size of the symbol.
-    /// </summary>
-    /// <exception cref="InvalidOperationException">
-    ///  Thrown when <see cref="Symbol"/> is <see langword="null"/>.
-    /// </exception>
+    /// <inheritdoc/>
     public Size? SymbolSize
     {
         get => _symbolSize;
 
-        set
-        {
-            if (_symbolSize == value)
-            {
-                return;
-            }
-
-            _symbolSize = value;
-            OnSymbolSizeChanged(EventArgs.Empty);
-        }
+        set => IToolStripItemSymbolProvider.SymbolSizeSetter(
+            provider: this,
+            onSymbolSizeChanged: OnSymbolSizeChanged,
+            value: value,
+            ref _symbolSize);
     }
 
     private bool ShouldSerializeSymbolSize() => _symbolSize.HasValue;
 
     private void ResetSymbolSize() => SymbolSize = null;
 
-    /// <summary>
-    ///  Gets or sets the offset of the symbol.
-    /// </summary>
+    /// <inheritdoc/>
     public Size SymbolOffset
     {
         get => _symbolOffset;
 
-        set
-        {
-            if (_symbolOffset == value)
-            {
-                return;
-            }
-
-            _symbolOffset = value;
-            OnSymbolOffsetChanged(EventArgs.Empty);
-        }
+        set => IToolStripItemSymbolProvider.SymbolOffsetSetter(
+            provider: this,
+            onSymbolOffsetChanged: OnSymbolOffsetChanged,
+            value: value,
+            ref _symbolOffset);
     }
 
-    private bool ShouldSerializeSymbolOffset() => _symbolOffset != default;
+    private bool ShouldSerializeSymbolOffset() 
+        => _symbolOffset != default;
 
-    private void ResetSymbolOffset() => SymbolOffset = default;
+    private void ResetSymbolOffset() 
+        => SymbolOffset = default;
 
     /// <inheritedDoc/>
     [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
@@ -204,64 +165,34 @@ public class ToolStripSymbolMenuItem : ToolStripMenuItem
     ///  Raises the <see cref="SymbolSizeChanged"/> event.
     /// </summary>
     /// <param name="e">An <see cref="EventArgs"/> containing the event data.</param>
-    protected virtual void OnSymbolSizeChanged(EventArgs e)
-    {
-        UpdateSymbolImageFactory();
-        SymbolSizeChanged?.Invoke(this, e);
-    }
+    protected virtual void OnSymbolSizeChanged(EventArgs e) 
+        => SymbolSizeChanged?.Invoke(this, e);
 
     /// <summary>
     ///  Raises the <see cref="SymbolChanged"/> event.
     /// </summary>
     /// <param name="e">An <see cref="EventArgs"/> containing the event data.</param>
-    protected virtual void OnSymbolChanged(EventArgs e)
-    {
-        UpdateSymbolImageFactory();
-        SymbolChanged?.Invoke(this, e);
-    }
+    protected virtual void OnSymbolChanged(EventArgs e) 
+        => SymbolChanged?.Invoke(this, e);
 
     /// <summary>
     ///  Raises the <see cref="SymbolColorChanged"/> event.
     /// </summary>
     /// <param name="e">An <see cref="EventArgs"/> containing the event data.</param>
-    protected virtual void OnSymbolColorChanged(EventArgs e)
-    {
-        UpdateSymbolImageFactory();
-        SymbolColorChanged?.Invoke(this, e);
-    }
+    protected virtual void OnSymbolColorChanged(EventArgs e) 
+        => SymbolColorChanged?.Invoke(this, e);
 
-    protected virtual void OnSymbolOffsetChanged(EventArgs e)
-    {
-        UpdateSymbolImageFactory();
-        SymbolOffsetChanged?.Invoke(this, e);
-    }
+    /// <summary>
+    ///  Raises the <see cref="SymbolOffsetChanged"/> event.
+    /// </summary>
+    /// <param name="e">An <see cref="EventArgs"/> containing the event data.</param>
+    protected virtual void OnSymbolOffsetChanged(EventArgs e) 
+        => SymbolOffsetChanged?.Invoke(this, e);
 
-    protected virtual void OnScalePercentageChanged(EventArgs e)
-    {
-        UpdateSymbolImageFactory();
-        ScalePercentageChanged?.Invoke(this, e);
-    }
-
-    private void UpdateSymbolImageFactory()
-    {
-        if (!(_symbolSource.HasSymbolValue) || !(_symbolSize.HasValue))
-        {
-            _symbolImageFactory = null;
-            Image = null!;
-            return;
-        }
-
-        _symbolImageFactory = new SymbolImageFactory(
-            (char) _symbolSource.Symbol,
-            _symbolSource.FontName,
-            _symbolSize.Value.Width,
-            _symbolSize.Value.Height,
-            _scalePercentage,
-            _symbolColor,
-            _transparentColor,
-            _symbolOffset.Width,
-            _symbolOffset.Height);
-
-        Image = _symbolImageFactory.SymbolImage;
-    }
+    /// <summary>
+    ///  Raises the <see cref="SymbolScalingChanged"/> event.
+    /// </summary>
+    /// <param name="e">An <see cref="EventArgs"/> containing the event data.</param>
+    protected virtual void OnSymbolScalingChanged(EventArgs e) 
+        => SymbolScalingChanged?.Invoke(this, e);
 }
