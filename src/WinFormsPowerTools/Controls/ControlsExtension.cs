0c707ac88ce504711b57ff96a5138347f12abd53
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2024-03-05 18:42:57 -0800

    Introduce Invokes.

--- src/WinFormsPowerTools/Controls/ControlsExtension.cs
+++ src/WinFormsPowerTools/Controls/ControlsExtension.cs
@@ -1,22 +1,109 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Runtime.ExceptionServices;
 
-namespace WinForms.PowerTools.Controls
+namespace WinForms.PowerTools.Controls;
+
+public static class ControlsExtension
 {
-    public static class ControlsExtension
+    /// <summary>
+    /// Executes an asynchronous function in a Task to avoid UI deadlocks, and blocks until the operation completes.
+    /// </summary>
+    /// <typeparam name="T">The type of the result returned by the function.</typeparam>
+    /// <param name="asyncFunc">The asynchronous function to execute.</param>
+    /// <returns>The result of the asynchronous operation.</returns>
+    public static T? AsyncInvoke<T>(this Control control, Func<Task<T>> asyncFunc)
     {
-        public static Task InvokeAsync(this Control control, Action action)
+        if (!control.IsHandleCreated)
+            throw new InvalidOperationException("Control handle not created.");
+
+        var tcs = new TaskCompletionSource<T>();
+
+        var callback = async () =>
         {
-            if (control.InvokeRequired)
+            try
             {
-                return control.InvokeAsync(action);
+                var result = await asyncFunc();
+                tcs.SetResult(result);
             }
-            else
+            catch (Exception ex)
             {
-                action();
-                return Task.CompletedTask;
+                tcs.SetException(ex);
             }
+        };
+
+        // Marshal the asyncFunc execution back to the UI thread
+        control.Invoke(async () => await Task.Run(callback));
+
+        T? result = default;
+
+        try
+        {
+            result = tcs.Task.Result;
         }
+        catch (Exception ex)
+        {
+            ExceptionDispatchInfo.Throw(ex);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    ///  Executes the specified asynchronous function on the thread that owns the control's handle.
+    /// </summary>
+    /// <param name="asyncAction">The asynchronous function to execute.</param>
+    /// <param name="args">The arguments to pass to the function.</param>
+    /// <returns>A task representing the operation.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the control's handle is not yet created.</exception>
+    public static Task<object> InvokeAsync(this Control control, Func<object[], Task<object>> asyncAction, params object[] args)
+    {
+        var tcs = new TaskCompletionSource<object>();
+
+        if (!control.IsHandleCreated)
+        {
+            tcs.SetException(new InvalidOperationException("Control handle not created."));
+            return tcs.Task;
+        }
+
+        control.BeginInvoke(new Action(async () =>
+        {
+            try
+            {
+                object result = await asyncAction(args).ConfigureAwait(true);
+                tcs.SetResult(result);
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        }));
+
+        return tcs.Task;
+    }
+
+    /// <summary>
+    ///  Executes the specified delegate on the thread that owns the control's handle.
+    /// </summary>
+    /// <param name="method">The delegate to execute.</param>
+    /// <param name="args">The arguments to pass to the delegate.</param>
+    /// <returns>A task representing the operation.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the control's handle is not yet created.</exception>
+    public static Task<object?> InvokeAsync(this Control control, Delegate method, params object[] args)
+    {
+        var tcs = new TaskCompletionSource<object?>();
+
+        control.BeginInvoke(new Action(() =>
+        {
+            try
+            {
+                object? result = method.DynamicInvoke(args);
+                tcs.SetResult(result);
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        }));
+
+        return tcs.Task;
     }
 }
--- src/WinFormsPowerToolsDemo/DocumentTestForm.cs
+++ src/WinFormsPowerToolsDemo/DocumentTestForm.cs
@@ -3,48 +3,64 @@ using System.Drawing.Drawing2D;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.Documents;
+using WinForms.PowerTools.Controls;
 
-namespace WinFormsPowerToolsDemo
+namespace WinFormsPowerToolsDemo;
+
+public partial class DocumentTestForm : Form
 {
-    public partial class DocumentTestForm : Form
+    public DocumentTestForm()
     {
-        public DocumentTestForm()
-        {
-            InitializeComponent();
-            Document doc = new();
-            documentControl1.MainDocument = doc;
+        InitializeComponent();
+        Document doc = new();
+        documentControl1.MainDocument = doc;
 
-            doc.SuspendUpdates();
-            doc.AddDocumentItem(new AsyncTestDocumentItem());
-            doc.ResumeUpdates();
-        }
+        doc.SuspendUpdates();
+        doc.AddDocumentItem(new AsyncTestDocumentItem());
+        doc.ResumeUpdates();
     }
 
-    public class AsyncTestDocumentItem : AsyncDocumentItem
+    private void button1_Click(object sender, System.EventArgs e)
     {
-        protected override async Task OnRenderAsync(PointF scrollOffset, IDeviceContext deviceContext)
+        label1.Text = label1.AsyncInvoke<string>(
+            async () =>
         {
-            if (deviceContext is not Graphics graphics)
+            for (int i = 0; i < 10; i++)
             {
-                return;
+                await Task.Delay(100);
             }
 
-            using Matrix matrix = new();
-            matrix.Translate(-scrollOffset.X, -scrollOffset.Y);
-            graphics.Transform = matrix;
+            throw new System.Exception("Test exception");
 
-            var documentSize = new SizeF(ParentDocument!.Width, ParentDocument!.Height);
-            for (int x = 0; x < 800; x += 10)
-            {
-                graphics.DrawLine(Pens.Black, new Point(0, 0), new Point(x, 600));
-            }
+            // return "Done!";
+        });
+    }
+}
 
-            await Task.FromResult(false);
-        }
-
-        protected override async Task VisibilityStateChangedAsync(VisibilityState visibilityState)
+public class AsyncTestDocumentItem : AsyncDocumentItem
+{
+    protected override async Task OnRenderAsync(PointF scrollOffset, IDeviceContext deviceContext)
+    {
+        if (deviceContext is not Graphics graphics)
         {
-            await Task.FromResult(false);
+            return;
         }
+
+        using Matrix matrix = new();
+        matrix.Translate(-scrollOffset.X, -scrollOffset.Y);
+        graphics.Transform = matrix;
+
+        var documentSize = new SizeF(ParentDocument!.Width, ParentDocument!.Height);
+        for (int x = 0; x < 800; x += 10)
+        {
+            graphics.DrawLine(Pens.Black, new Point(0, 0), new Point(x, 600));
+        }
+
+        await Task.FromResult(false);
+    }
+
+    protected override async Task VisibilityStateChangedAsync(VisibilityState visibilityState)
+    {
+        await Task.FromResult(false);
     }
 }
