ceea0496e01e38c52c10394a466a8b7254a58a09
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2024-07-03 00:27:14 -0700

    Update project structure.

--- src/WinForms.PowerTools.Win32/PInvokeCore.cs
+++ src/WinForms.PowerTools.Win32/PInvokeCore.cs
@@ -1,7 +1,13 @@
-﻿namespace WinForms.PowerTools.Win32
-{
-    public partial class PInvokeCore
-    {
+﻿using Windows.Win32.Foundation;
 
+namespace WinForms.PowerTools.Win32
+{
+    public static class PInvokeExtension
+    {
+        public static COLORREF ToCOLORREF(this System.Drawing.Color color)
+        {
+            COLORREF colorRef = new((uint)(color.R | color.G << 8 | color.B << 16));
+            return colorRef;
+        }
     }
 }
